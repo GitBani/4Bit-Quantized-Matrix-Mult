c87d3e069675a7cc0384e1bde3673852624784d3
Bani Somo <banisomo2003@gmail.com>
2025-02-02 23:11:14 -0600

    Add function to pack 4-bit numbers into a byte

--- src/quantization/mod.rs
+++ src/quantization/mod.rs
@@ -5,3 +5,9 @@ pub trait Quantizer4Bit {
     fn quantize(&self, real_val: f32) -> i8;
     fn dequantize(&self, q_val: i8) -> f32;
 }
+
+pub fn quantize_and_pack(quantizer: &impl Quantizer4Bit, v1: f32, v2: f32) -> i8 {
+    let q1 = quantizer.quantize(v1);
+    let q2 = quantizer.quantize(v2);
+    (q2 << 4) + q1
+}
