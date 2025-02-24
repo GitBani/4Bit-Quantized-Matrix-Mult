b5f339d9dd45d32efcf83b38dcc3e02db29d45c0
Bani Somo <banisomo2003@gmail.com>
2025-02-23 21:00:54 -0600

    Set up BLAS crate

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,22 @@
+use blas::sgemm;
+
 mod matrices;
 mod quantization;
 
-use quantization::affine_quantizer::AffineQuantizer;
-// use quantization::affine_symmetric::AffineSymmetric;
-use quantization::Quantizer4Bit;
-use rand::Rng;
-
 fn main() {
-    // Just some temporary testing
-    // let mut rng = rand::rng();
-    // let min_val = -100.0;
-    // let max_val = 100.0;
-    // let unsigned_scheme = AffineQuantizer::new(min_val, max_val);
-    // let symmetric_scheme = AffineSymmetric::new(min_val, max_val);
+    // Testing out BLAS
 
-    // for _ in 0..20 {
-    //     let f = rng.random_range(min_val..=max_val);
+    let (m, n, k) = (2, 2, 3);
+    let a: Vec<f32> = vec![7.0, 10.0, 8.0, 11.0, 9.0, 12.0];
+    let b: Vec<f32> = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
 
-    //     let uq = unsigned_scheme.quantize(f);
-    //     let udq = unsigned_scheme.dequantize(uq);
-    //     let symq = symmetric_scheme.quantize(f);
-    //     let symdq = symmetric_scheme.dequantize(symq);
+    let mut c: Vec<f32> = vec![0.0; 4];
+    unsafe {
+        sgemm(b'N', b'N', m, n, k, 1.0, &a, m, &b, k, 1.0, &mut c, m);
+    }
 
-    //     println!("Float: {f}");
-    //     println!("Unsigned q: {uq}, back: {udq}",);
-    //     println!("Symm q: {symq}, back: {symdq}",);
-    //     println!("")
-    // }
+    let expected: Vec<f32> = vec![50.0, 68.0, 122.0, 167.0];
+
+    dbg!(&c);
+    assert_eq!(&c, &expected);
 }
